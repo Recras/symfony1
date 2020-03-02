8665332369e59cc60a004cb7b4417291426eb055
Tijmen Klein <tijmen@recras.nl>
2020-03-02 12:14:34 +0100

    WIP: don't let sfAutoloadAgain do anything

--- lib/autoload/sfAutoloadAgain.class.php
+++ lib/autoload/sfAutoloadAgain.class.php
@@ -10,7 +10,7 @@
 
 /**
  * Autoload again for dev environments.
- * 
+ *
  * @package    symfony
  * @subpackage autoload
  * @author     Kris Wallsmith <kris.wallsmith@symfony-project.com>
@@ -21,13 +21,9 @@ class sfAutoloadAgain
   static protected
     $instance = null;
 
-  protected
-    $registered = false,
-    $reloaded   = false;
-
   /**
    * Returns the singleton autoloader.
-   * 
+   *
    * @return sfAutoloadAgain
    */
   static public function getInstance()
@@ -49,64 +45,23 @@ class sfAutoloadAgain
 
   /**
    * Reloads the autoloader.
-   * 
+   *
    * @param  string $class
-   * 
+   *
    * @return boolean
    */
   public function autoload($class)
   {
-    // only reload once
-    if ($this->reloaded)
-    {
-      return false;
-    }
-
-    $autoloads = spl_autoload_functions();
-
-    // as of PHP 5.2.11, spl_autoload_functions() returns the object as the first element of the array instead of the class name
-    if (version_compare(PHP_VERSION, '5.2.11', '>='))
-    {
-      foreach ($autoloads as $position => $autoload)
-      {
-        if (is_array($autoload) && $this === $autoload[0])
-        {
-          break;
-        }
-      }
-    }
-    else
-    {
-      $position  = array_search(array(__CLASS__, 'autoload'), $autoloads, true);
-    }
-
-    if (isset($autoloads[$position + 1]))
-    {
-      $this->unregister();
-      $this->register();
-
-      // since we're rearranged things, call the chain again
-      spl_autoload_call($class);
-
-      return class_exists($class, false) || interface_exists($class, false);
-    }
-
-    $autoload = sfAutoload::getInstance();
-    $autoload->reloadClasses(true);
-
-    $this->reloaded = true;
-
-    return $autoload->autoload($class);
   }
 
   /**
    * Returns true if the autoloader is registered.
-   * 
+   *
    * @return boolean
    */
   public function isRegistered()
   {
-    return $this->registered;
+    return true;
   }
 
   /**
@@ -114,11 +69,6 @@ class sfAutoloadAgain
    */
   public function register()
   {
-    if (!$this->isRegistered())
-    {
-      spl_autoload_register(array($this, 'autoload'));
-      $this->registered = true;
-    }
   }
 
   /**
@@ -126,7 +76,5 @@ class sfAutoloadAgain
    */
   public function unregister()
   {
-    spl_autoload_unregister(array($this, 'autoload'));
-    $this->registered = false;
   }
 }
