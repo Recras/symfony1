b14289b2c092f6bce9c660cd94c012813d6af756
Sander de Jong <sander@recras.nl>
2022-06-27 16:25:46 +0200

    Creation of dynamic property is deprecated in PHP 8.2

--- lib/action/sfActions.class.php
+++ lib/action/sfActions.class.php
@@ -4,7 +4,7 @@
  * This file is part of the symfony package.
  * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
  * (c) 2004-2006 Sean Kerr <sean@code-box.org>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -34,6 +34,10 @@ abstract class sfActions extends sfAction
    *
    * @see sfAction
    */
+
+  protected array $data = [];
+  protected string $output = '';
+
   public function execute($request)
   {
     // dispatch action
